512c177004902f482c0550e68943320a39332ffa
LEELAVATHY M <157588125+Leelavathy-M@users.noreply.github.com>
2024-04-17 14:31:38 +0530

    Add files via upload

---
+++ rout.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import {Route, Routes} from 'react-router-dom';
+// import Bg from './sections/Bg/Bg.js'
+// import Product from './sections/product/product.js';
+// import NewArrival from './sections/NewArrivals/NewArrival.js';
+// import {Fragrance} from './sections/pages/Fragy/Fragrance.js'
+// import {Combo} from './sections/pages/Combo.js'
+// import {Gift} from './sections/pages/Gift.js'
+// import {Body} from './sections/pages/Body/Body.js'
+// const Rout = ({Fragrance, setFragrance, detail, view}) => {
+//   return (
+//     <>
+//     <Routes>
+//       <Route exact path="/" element={<><Bg/> <Product /> <NewArrival /></>}/>
+//       <Route path="/fragrance" element={<Fragrance setFragrance={setFragrance} detail={detail} view={view}/>} />
+//       <Route path="/body" element={<Body/>} />
+//       <Route path="/combo" element={<Combo/>} />
+//       <Route path="/gift" element={<Gift/>} />
+//       </Routes>
+//     </>
+//   )
+// }
+
+// export default Rout
